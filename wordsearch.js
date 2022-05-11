49efe8c251b59b74c4aca369e4d701751fefe878
minhvo1 <minhvo199@gmail.com>
2022-05-11 21:17:50 +0000

    fixed return bug and implemented vertical search

--- wordsearch.js
+++ wordsearch.js
@@ -3,6 +3,27 @@ const wordSearch = (letters, word) => {
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+    // transposition
+    let transposedLetters = [];
+    let numRows = letters.length;
+    let numColumns = letters[0].length;
+  
+    for (let i = 0; i < numColumns; i++) {
+        transposedLetters.push([]);
+    }
+    // for each row
+    for (let i = 0; i < numRows; i++) {
+        // for each column
+        for (let j = 0; j < numColumns; j++) {
+            transposedLetters[j].push(letters[i][j]);
+         }
+    }
+    const verticalJoin = transposedLetters.map(ls => ls.join(''))
+    for (l of verticalJoin) {
+        if (l.includes(word)) return true
+    }
+    return false;
+
 }
 
 module.exports = wordSearch
